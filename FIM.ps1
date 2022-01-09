961fc31ff6ac10f554822125dc4f0c680ec34ec2
Nikhil Thakur <96281583+NikhilThakur-IT@users.noreply.github.com>
2022-01-09 12:47:37 +1100

    Implementing hashing algorithm
    - Added to collect all files in the target folder
    - Created a function that hashes and write to baseline.txt
    - Created an erase baseline function to start monitor different files

--- FIM.ps1
+++ FIM.ps1
@@ -1,31 +1,60 @@
-﻿
-Write-Host "" <#For UI reasons#>
-                
-Write-Host "What would you like to do?"
-Write-Host "A) Collect new Baseline?"
-Write-Host "B) Begin monitoring files with saved Baseline?"
-Write-Host "" <#For UI reasons#>
+﻿<# Author Nikhil Thakur
+  - Write-Host "" are used For UI reasons#>  
 
-$response= Read-Host -Prompt "Please enter A or B"
-Write-Host "" <#For UI reasons#>
+
+# Method Calculate-File-Hash uses SHA512 algorithm to get the hash from the target files
 
 Function Calculate-File-Hash($filepath) {
    $filehash = Get-FileHash -Path $filepath -Algorithm SHA512
    return $filehash
 }
 
+Function Erase-Baseline-if-exists(){
+    $baselineExists = Test-Path -Path .\baseline.txt
+   
+    if ($baselineExists){
+    Remove-Item -Path .\baseline.txt
+    }
+}
+
+Write-Host ""           
+Write-Host "What would you like to do?"
+Write-Host "" 
+Write-Host "    A) Collect new Baseline?"
+Write-Host "    B) Begin monitoring files with saved Baseline?"
+Write-Host ""
+$response= Read-Host -Prompt "Please enter A or B"
+Write-Host ""
+
+
 
 <# To test the hashing algorithm #>
-<#Calculate-File-Hash "C:\Users\niket\Documents\- IT\Projects\FIle Integrity Monitor\test1.txt"#> 
+<#Calculate-File-Hash "C:\Users\niket\Documents\- IT\Projects\FIle Integrity Monitor\Files\Test1.txt"#>
                                                                                              
-                                                                                             
+                                                                                        
 
 if ($response -eq "A".ToUpper()){
-  #Calculate Hash from the target files amd store in baseline.txt
-  Write-Host "Calculate Hashes, make new baseline.txt" -ForegroundColor Yellow
+  #Delete Baseline.txt if it already exists
+  Erase-Baseline-if-exists
 
+  #Calculate Hash from the target files amd store in baseline.txt
+  
+  #Collect all files in the target folder
+  $files = Get-ChildItem -Path .\Files
+  
+  #For file, Calculate the hash and write to baseline.txt
+  foreach ($f in $files){
+  $hash = Calculate-File-Hash $f.FullName
+   "$($hash.Path)|$($hash.Hash)" | Out-File -FilePath .\baseline.txt -Append
+      }
+   write-Host "Hashes have been calculated and added to baseline.txt." -ForegroundColor Yellow
+   .\FIM.ps1 -Append
 }
+
 elseif ($response -eq "B".ToUpper()){
+     # Load file|hash from baseline.txt and store them in a dictionary
+    
+
 #Begin monitoring files with saved Baseline
 write-Host "Read existing baseline.txt, begin monitoring files." -ForegroundColor Yellow
 }
